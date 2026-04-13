8451671792028d560fe286d67c5713a9e6b075d9
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-04-13 13:44:39 +0200

    added sql server dependencies and WebAPI class library

--- CarRental.Application/obj/Debug/net8.0/CarRental.Application.AssemblyInfo.cs
+++ CarRental.Application/obj/Debug/net8.0/CarRental.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a625dfa9cf8819ca8c141cc0d0b96331389fd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
